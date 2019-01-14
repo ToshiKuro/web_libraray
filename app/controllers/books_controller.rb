50d4f93f1cac2a82f69ba17ad0c67c43526eabf4
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-01-14 20:37:59 +0900

    changed book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
-    @book.name = params[:book][:name]
-    @book.author = params[:book][:author]
-    @book.date_of_issue = params[:book][:date_of_issue]
-    @book.summary = params[:book][:summary]
-    @book.save
+    @book = Book.create(book_params)
+    # @book = Book.new(params[:book])
+    # @book.save   
+    # @book = Book.new
+    # @book.name = params[:book][:name]
+    # @book.author = params[:book][:author]
+    # @book.date_of_issue = params[:book][:date_of_issue]
+    # @book.summary = params[:book][:summary]
+    # @book.save
     redirect_to '/books/index'  
   end
 
@@ -22,5 +25,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name,:author,:date_of_issue,:summary)
   end
 end
